7bf29ffddc9ebbf465003be2c48dfabe249aaf1a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-10-25 20:55:55 +0200

    Fix flawless mixin

--- src/main/java/wdfeer/esoterica_origins/Initializer.java
+++ src/main/java/wdfeer/esoterica_origins/Initializer.java
@@ -2,8 +2,10 @@ package wdfeer.esoterica_origins;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,10 +16,11 @@ public class Initializer implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final Flawless FLAWLESS = new Flawless();
+    public static RegistryEntry<StatusEffect> flawlessEntry;
 
     @Override
     public void onInitialize() {
-        Registry.register(Registries.STATUS_EFFECT, Identifier.of(MOD_ID, "flawless"), FLAWLESS);
+        flawlessEntry = Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(MOD_ID, "flawless"), FLAWLESS);
         LOGGER.info("Esoterica Origins loaded!");
     }
 }
--- src/main/java/wdfeer/esoterica_origins/mixin/FlawlessMixin.java
+++ src/main/java/wdfeer/esoterica_origins/mixin/FlawlessMixin.java
@@ -3,6 +3,7 @@ package wdfeer.esoterica_origins.mixin;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,11 +14,11 @@ import wdfeer.esoterica_origins.Initializer;
 @Mixin(LivingEntity.class)
 public abstract class FlawlessMixin {
     @Shadow
-    public abstract boolean hasStatusEffect(StatusEffect effect);
+    public abstract boolean hasStatusEffect(RegistryEntry<StatusEffect> effect);
 
     @Inject(method = "modifyAppliedDamage", at = @At("RETURN"), cancellable = true)
     private void modifyAppliedDamage(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
-        if (hasStatusEffect(Initializer.FLAWLESS) && cir.getReturnValue() > 1) {
+        if (hasStatusEffect(Initializer.flawlessEntry) && cir.getReturnValue() > 1) {
             cir.setReturnValue(1f);
         }
     }
