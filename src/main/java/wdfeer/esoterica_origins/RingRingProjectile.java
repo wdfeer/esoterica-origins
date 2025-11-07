a2555c5d1cb3322ed78f175e98f5852cb3fd4cf3
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-11-07 17:20:59 +0100

    Implement RingRingProjectile homing

--- src/main/java/wdfeer/esoterica_origins/RingRingProjectile.java
+++ src/main/java/wdfeer/esoterica_origins/RingRingProjectile.java
@@ -1,18 +1,21 @@
 package wdfeer.esoterica_origins;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.*;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.entity.mob.Monster;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 import java.util.Random;
+import java.util.UUID;
 
 public class RingRingProjectile extends ProjectileEntity {
     static Random random = new Random();
@@ -23,7 +26,7 @@ public class RingRingProjectile extends ProjectileEntity {
 
     public static final EntityType<RingRingProjectile> ENTITY_TYPE =
             FabricEntityTypeBuilder.create(SpawnGroup.MISC, RingRingProjectile::new)
-                    .dimensions(EntityDimensions.fixed(0.25f, 0.25f))
+                    .dimensions(EntityDimensions.fixed(0.4f, 0.4f))
                     .build();
 
     public RingRingProjectile(EntityType<? extends ProjectileEntity> entityType, World world) {
@@ -38,10 +41,43 @@ public class RingRingProjectile extends ProjectileEntity {
 
     @Override
     public void tick() {
-        if (getWorld().isClient) {
+        var world = getWorld();
+        if (world.isClient) {
             var color = new Vector3f(0.6f + random.nextFloat() * 0.2f, 0f, 0f);
-            getWorld().addParticle(new DustParticleEffect(color, 2f), this.getX(), this.getY(), this.getZ(), 0, 0, 0);
+            world.addParticle(new DustParticleEffect(color, 2f), this.getX(), this.getY(), this.getZ(), 0, 0, 0);
+        } else if (world instanceof ServerWorld serverWorld) {
+            if (targetUUID == null) {
+                for (Entity e : serverWorld.iterateEntities()) {
+                   if (e instanceof Monster) {
+                      targetUUID = e.getUuid();
+                   }
+                }
+                return;
+            }
+
+            Entity target = serverWorld.getEntity(targetUUID);
+            if (target == null || target.isRemoved() || !target.isAlive()) {
+                kill();
+                return;
+            }
+
+            var direction = target.getPos().subtract(getPos()).normalize();
+            addVelocity(direction.multiply(0.01));
         }
+
         super.tick();
+    }
+
+    @Nullable
+    private UUID targetUUID = null;
+
+    @Override
+    protected void readCustomDataFromNbt(NbtCompound nbt) {
+        targetUUID = nbt.getUuid("target");
+    }
+
+    @Override
+    protected void writeCustomDataToNbt(NbtCompound nbt) {
+        nbt.putUuid("target", targetUUID);
     }
 }
