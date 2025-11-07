37e11faa06f289ac005575f9500062f094e51ba5
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-11-07 16:54:55 +0100

    Fix FlawlessMixin

--- src/main/java/wdfeer/esoterica_origins/mixin/FlawlessMixin.java
+++ src/main/java/wdfeer/esoterica_origins/mixin/FlawlessMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import wdfeer.esoterica_origins.Initializer;
+import wdfeer.esoterica_origins.Flawless;
 
 @Mixin(LivingEntity.class)
 public abstract class FlawlessMixin {
@@ -17,7 +17,7 @@ public abstract class FlawlessMixin {
 
     @Inject(method = "modifyAppliedDamage", at = @At("RETURN"), cancellable = true)
     private void modifyAppliedDamage(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
-        if (hasStatusEffect(Initializer.FLAWLESS) && cir.getReturnValue() > 1) {
+        if (hasStatusEffect(Flawless.INSTANCE) && cir.getReturnValue() > 1) {
             cir.setReturnValue(1f);
         }
     }
