50a6aa747818e990ee7250372ff90692ff487e61
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-11-18 09:26:12 +0100

    Fix graze effect

--- src/main/java/wdfeer/esoterica_origins/mixin/AppliedDamageMixin.java
+++ src/main/java/wdfeer/esoterica_origins/mixin/AppliedDamageMixin.java
@@ -2,11 +2,10 @@ package wdfeer.esoterica_origins.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.registry.tag.DamageTypeTags;
-import org.spongepowered.asm.mixin.Final;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,27 +14,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import wdfeer.esoterica_origins.Flawless;
 import wdfeer.esoterica_origins.Graze;
 
-import java.util.Map;
-
 @Mixin(LivingEntity.class)
 public abstract class AppliedDamageMixin {
     @Shadow
     public abstract boolean hasStatusEffect(StatusEffect effect);
 
     @Shadow
-    @Final
-    private Map<StatusEffect, StatusEffectInstance> activeStatusEffects;
+    public abstract boolean removeStatusEffect(StatusEffect type);
+
+    @Shadow
+    public abstract boolean addStatusEffect(StatusEffectInstance effect);
+
+    @Shadow
+    @Nullable
+    public abstract StatusEffectInstance getStatusEffect(StatusEffect effect);
 
     @Inject(method = "modifyAppliedDamage", at = @At("RETURN"), cancellable = true)
     private void modifyAppliedDamage(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
         if (hasStatusEffect(Graze.INSTANCE) && cir.getReturnValue() > 0 && !source.isIn(DamageTypeTags.BYPASSES_EFFECTS)) {
-            var instance = activeStatusEffects.get(Graze.INSTANCE);
+            var instance = getStatusEffect(Graze.INSTANCE);
+            assert instance != null;
             if (instance.getAmplifier() > 1) {
-                activeStatusEffects.replace(Graze.INSTANCE, new StatusEffectInstance(Graze.INSTANCE,
+                removeStatusEffect(Graze.INSTANCE);
+                addStatusEffect(new StatusEffectInstance(Graze.INSTANCE,
                         instance.getDuration(),
                         instance.getAmplifier() - 1));
             } else {
-                activeStatusEffects.remove(Graze.INSTANCE);
+                removeStatusEffect(Graze.INSTANCE);
             }
 
             cir.setReturnValue(0f);
