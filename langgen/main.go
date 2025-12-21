81a92742b165cc01cf31fe295de702cf5916c240
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-12-21 20:56:48 +0100

    Init creating an en_us.json localization file

---
+++ langgen/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"strings"
+)
+
+const (
+	originDir  = "../src/main/resources/data/esoterica-origins/origins/"
+	powerDir   = "../src/main/resources/data/esoterica-origins/powers/"
+	outputFile = "./en_us.json"
+)
+
+func main() {
+	files, _ := os.ReadDir(originDir)
+
+	origins := make([]Origin, len(files))
+	for i, filename := range files {
+		origins[i] = parseOrigin(originDir + filename.Name())
+	}
+
+	lang := buildLang(origins)
+	bytes, _ := json.Marshal(lang)
+	os.WriteFile(outputFile, bytes, 0644)
+}
+
+type Origin struct {
+	name        string
+	description string
+	powers      []Power
+}
+
+func parseOrigin(path string) Origin {
+	object := readJson(path)
+
+	if object["name"] == nil {
+		panic("Failed reading origin at\"" + path + "\", name not found!")
+	}
+	if object["description"] == nil {
+		panic("Failed reading origin at\"" + path + "\", description not found!")
+	}
+	if object["powers"] == nil {
+		panic("Failed reading origin at\"" + path + "\", powers not found!")
+	}
+
+	powerNames := object["powers"].([]any)
+	powers := []Power{}
+	for _, identifier := range powerNames {
+		split := strings.Split(identifier.(string), ":")
+		if split[0] == "esoterica-origins" {
+			powers = append(powers, parsePower(split[1]))
+		}
+	}
+
+	return Origin{
+		name:        object["name"].(string),
+		description: object["description"].(string),
+		powers:      powers,
+	}
+}
+
+type Power struct {
+	hidden      bool
+	name        string
+	description string
+}
+
+func parsePower(name string) Power {
+	path := powerDir + name + ".json"
+	object := readJson(path)
+
+	if object["hidden"] == true {
+		return Power{
+			hidden: true,
+		}
+	}
+
+	if object["name"] == nil {
+		panic("Failed reading power at\"" + path + "\", name not found!")
+	}
+	if object["description"] == nil {
+		panic("Failed reading power at\"" + path + "\", description not found!")
+	}
+
+	return Power{
+		name:        object["name"].(string),
+		description: object["description"].(string),
+	}
+}
+
+func readJson(path string) map[string]any {
+	var object map[string]any
+	data, err := os.ReadFile(path)
+	if err != nil {
+		panic("Failed to read file at \"" + path + "\":" + err.Error())
+	}
+	err = json.Unmarshal(data, &object)
+	if err != nil {
+		panic("Failed to unmarshal json at \"" + path + "\":" + err.Error())
+	}
+	return object
+}
+
+func buildLang(origins []Origin) map[string]any {
+	// TODO: create a language localization file from `origins`
+}
